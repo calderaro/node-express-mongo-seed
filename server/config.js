eef1ce569e73ecf446ee4e126a7cfd7f67a2daa1
Angel Calderaro <calderaro94@gmail.com>
2015-10-28 23:23:27 -0430

    adding a config file

---
+++ server/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  HTTP_PORT: 3000,
+  DB_URI: "mongodb://localhost/preorders",
+
+}
--- server/server.js
+++ server/server.js
@@ -9,10 +9,8 @@ let http = require("http"),
 	chalk = require("chalk"),
 	bodyParser = require('body-parser'),
 	app = express(),
-	server = http.createServer(app)
-
-const dbURI = "mongodb://localhost/preorders"; 
-const port = 3000;
+	server = http.createServer(app),
+	config = require("./config");
 
 app.set('views', path.join(__dirname,"./views"));
 app.set('view engine', 'jade');
@@ -26,25 +24,27 @@ app.use((req, res, next) => {
 });
 app.use(require("./routes"));
 
-// Create the database connection 
-mongoose.connect(dbURI, function(err){
-    
-    if (err) return console.log(chalk.red(err));
-    
-    // listen for connection throws an error
-	mongoose.connection.on('error', (err) => { console.log(chalk.red("Mongoose default connection error: " + err)) }); 
-	
+// Create the database connection
+console.log(config);
+mongoose.connect(config.DB_URI, function(err){
+
+  if (err) return console.log(chalk.red(err));
+
+  // listen for connection throws an error
+	mongoose.connection.on('error', (err) => { console.log(chalk.red("Mongoose default connection error: " + err)) });
+
 	// listen for connection is disconnected
 	mongoose.connection.on('disconnected', () => { console.log(chalk.red("Mongoose default connection disconnected")) });
-	
+
+	//register models
+	require("./models/");
+
 	// log the connection
-    console.log(chalk.green("Mongoose connection open to " + dbURI));
+  console.log(chalk.green("Mongoose connection open to " + config.DB_URI));
 
-    require("./models/");
-
-    // if not errors open the http server
-	server.listen(port,(err) => {
+  // if not errors open the http server
+	server.listen(config.HTTP_PORT ,(err) => {
 		if(err) console.log(chalk.red(err));
-		else console.log(chalk.green("http server listening port " + port));
+		else console.log(chalk.green("http server listening port " + config.HTTP_PORT));
 	});
 });
